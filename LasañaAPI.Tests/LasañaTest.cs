d18d539fe168a1595965d8baa960fcf55bdfe113
Quique Vázquez Peñamaría <vazquez.penamaria.enrique.ald@gmail.com>
2023-04-04 20:06:18 +0200

    Lasaña C#

---
+++ LasañaAPI.Tests/LasañaTest.cs
@@ -0,0 +1,5 @@
+﻿namespace LasañaAPI.Tests;
+public class LasañaTest
+{
+
+}
---
+++ LasañaAPI/Lasaña.cs
@@ -0,0 +1,35 @@
+﻿namespace LasañaAPI;
+public class Lasaña
+{
+    // TODO: define the 'ExpectedMinutesInOven()' method
+
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
+
+    // TODO: define the 'RemainingMinutesInOven()' method
+
+    public int RemainingMinutesInOven(int actualMinutes)
+    {
+        int expectedMinutes = ExpectedMinutesInOven();
+        return expectedMinutes - actualMinutes;
+    }
+
+    // TODO: define the 'PreparationTimeInMinutes()' method
+
+    public int PreparationTimeInMinutes(int numberOfLayers)
+    {
+        int preparationTimePerLayer = 2;
+        return numberOfLayers * preparationTimePerLayer;
+    }
+
+    // TODO: define the 'ElapsedTimeInMinutes()' method
+
+    public int ElapsedTimeInMinutes(int numberOfLayers, int actualMinutesInOven)
+    {
+        int preparationTime = PreparationTimeInMinutes(numberOfLayers);
+        int expectedMinutesInOven = ExpectedMinutesInOven();
+        return preparationTime + actualMinutesInOven;
+    }
+}
